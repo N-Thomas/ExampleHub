322e3961f6ab4abbcf60b03f35aa82b2ac18117c
N-Thomas <nick.thomas@utah.edu>
2016-04-21 17:10:24 -0600

    moved conn to function

--- Model/hidden/db.php
+++ Model/hidden/db.php
@@ -4,11 +4,14 @@ $username = "Grad_Application";
 $password = "598254479";
 $dbName = "ExampleHub";
 
-try{
-        $conn = new PDO("mysql:host=$serverName;dbname=$dbName", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch(PDOException $e){
-        echo $e->getMessage();
+public function connect(){
+	try{
+	        $conn = new PDO("mysql:host=$serverName;dbname=$dbName", $username, $password);
+	        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	        return $conn;
+	}
+	catch(PDOException $e){
+	        return $e->getMessage();
+	}
 }
 ?>
