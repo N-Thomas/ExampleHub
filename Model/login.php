cd82e4d9e90ade8347344caf6fbbdf471cfafc76
James <jameskhaws@hotmail.com>
2016-04-26 00:53:23 -0600

    change password verification

--- Model/login.php
+++ Model/login.php
@@ -19,7 +19,7 @@ function logIn( $user, $password){
             $hashedPassword = $row['Password'];
 
             //Validate
-            if (computeHash($password, $hashedPassword) == $hashedPassword){
+            if (password_verify($password, $hashedPassword)){
                 session_start();
                 $_SESSION['id'] = $row['ID'];
                 $_SESSION['username'] = $row['UserName'];
