37cf0ee3e5a9fc47301056098e9d617bccfbe4d0
James <jameskhaws@hotmail.com>
2016-04-25 22:56:17 -0600

    remember to require model for session.php

--- Controller/session.php
+++ Controller/session.php
@@ -9,7 +9,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
+require_once '../Model/login.php';
 $username = strip_tags($_POST['username']);//prevent sql injection.
 $password = strip_tags($_POST['hash']);
 
