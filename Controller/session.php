7c9f611644c608e0a4f22e5411c5563923501f30
James <jameskhaws@hotmail.com>
2016-04-25 22:42:27 -0600

    Rename make cookie to session

---
+++ Controller/session.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mitchell
+ * Date: 4/24/2016
+ * Time: 10:13 PM
+ */
+session_start();
+
+$username = strip_tags($_POST['username']);//prevent sql injection.
+$password = strip_tags($_POST['hash']);
+
+//echo $username."us";
+$username = stripslashes($username);
+$password = stripslashes($password);
+//echo $username."us";
+
+login($username, $password);
+
+
+
+
+header("Location: PersonalBoard.php");
+?>
+
