3ab4badcf121339af4dc5d5905f43128fdc5d1cd
James <jameskhaws@hotmail.com>
2016-04-26 20:37:55 -0600

    change verify slightly

--- Controller/verify.php
+++ Controller/verify.php
@@ -8,8 +8,8 @@ if(!isset($_SESSION['id'])){//verify that they are signed in.
 	header("Location: Login.php");
 }
 else{
-  
-	$user = populate($_SESSION['id']);
+    $user = new User($_SESSION['id']);
+	$user->populate($_SESSION['id']);
 }
 
 ?>
