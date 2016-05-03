52d1d8b512ac76f7986bc5a5f835e4885d41abce
James <jameskhaws@hotmail.com>
2016-05-03 13:23:21 -0600

    semicolon

--- Model/post.php
+++ Model/post.php
@@ -153,7 +153,7 @@ class Post
 			
 			//increment/decrement post score
 			if($vote > 0) {
-				$stmt = $db->prepare("UPDATE Post SET Score= Score+1 WHERE ID=$idPost")
+				$stmt = $db->prepare("UPDATE Post SET Score= Score+1 WHERE ID=$idPost");
 				$db->beginTransaction();
 				$db->commit();
 			}
