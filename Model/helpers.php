8fc62deff99a0f0efac5cfb85bb8b25aa047dd03
N-Thomas <nick.thomas@utah.edu>
2016-05-03 15:23:20 -0600

    added userQuetions function

--- Model/helpers.php
+++ Model/helpers.php
@@ -23,4 +23,25 @@ function frontpage( $number ){
 
 }
 
+function userquestions( $number, $id ){
+    $results = [];
+
+    require 'hidden/db.php';
+    $query = "SELECT ID FROM Post WHERE Parent=0, UserID=" . $id .  " ORDER BY Date DESC LIMIT " . $number;
+
+    $statement = $db->prepare( $query );
+    $statement->execute(  );
+
+    $result    = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($result as $row){
+        $toAdd = new Post();
+        $toAdd->populate($row['ID']);
+        array_push($results, $toAdd);
+    }
+
+    return $results;
+
+}
+
 ?>
