60dc312053b98ce18dac7c0c049c9232a6e13b9d
aaronmcghie <amcghie54@gmail.com>
2016-05-03 15:02:39 -0600

    added same code to personal board

--- Controller/PersonalBoard.php
+++ Controller/PersonalBoard.php
@@ -7,6 +7,31 @@ require_once '../Model/login.php';
 require '../Model/user.php';
 require '../Model/post.php';
 
+//generates html for each post   Written by Aaron McGhie
+$html = "";
+//todo replace next line with function that pulls posts by user
+$posts = frontpage(10);
+foreach($posts as $p){
+	$postId = $p->getId();
+	$postString = <<<END
+		<li>
+            <div class="container">
+                <h2>$p->category</h2>
+                <div class="well">
+					<div class = "row">
+						<div class = "col-lg-10">$p->title</div>
+					
+						<div class = "col-lg-2">
+						<a href = "QA.php?id=$postId" class = "btn btn-default">View Question</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</li>
+END;
+	$html.=$postString;
+}
+
 require_once 'verify.php';
 require_once '../View/PersonalBoard.php';
 
